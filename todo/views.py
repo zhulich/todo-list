39e5933a62418bcd685430a7b975865c8abd6ed8
Yevhenii Davydenko <akazhulich@gmail.com>
2022-11-14 11:12:33 +0200

    For Task and Tag create views: list, create, update, delete

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,49 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
+
+from todo.models import Task, Tag
+
 
 # Create your views here.
+
+class TaskListView(generic.ListView):
+    model = Task
+
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("practice:task-list")
+
+
+class TaskUpdateView(generic.UpdateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("practice:task-list")
+
+
+class TaskDeleteView(generic.DeleteView):
+    model = Task
+    success_url = reverse_lazy("practice:task-list")
+
+
+class TagListView(generic.ListView):
+    model = Tag
+
+
+class TagCreateView(generic.CreateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("practice:tag-list")
+
+
+class TagUpdateView(generic.UpdateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("practice:tag-list")
+
+
+class TagDeleteView(generic.DeleteView):
+    model = Tag
+    success_url = reverse_lazy("practice:tag-list")
