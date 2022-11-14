f84b91478e055d598100901f4757f0b227d4d161
Yevhenii Davydenko <akazhulich@gmail.com>
2022-11-14 11:12:59 +0200

    Add path's for all created views

--- practice/urls.py
+++ practice/urls.py
@@ -16,6 +16,17 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from todo.views import TaskListView, TaskCreateView, TaskUpdateView, TaskDeleteView, TagListView, TagCreateView, \
+    TagUpdateView, TagDeleteView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", TaskListView.as_view(), name="task-list"),
+    path("create/", TaskCreateView.as_view(), name="task-create"),
+    path("<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
+    path("<int:pk>/delete/", TaskDeleteView.as_view, name="task-delete"),
+    path("tags/", TagListView.as_view(), name="tag-list"),
+    path("tags/create/", TagCreateView.as_view(), name="tag-create"),
+    path("tags/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
+    path("tags/<int:pk>/delete/", TagDeleteView.as_view, name="tag-delete")
 ]
