b15b51930ff61a6529dbaf6578f7c33fdf46b4fa
Yevhenii Davydenko <akazhulich@gmail.com>
2022-11-14 14:45:06 +0200

    Tag str method and ordering Task

--- todo/models.py
+++ todo/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Tag(models.Model):
     name = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.name
+
 
 class Task(models.Model):
     content = models.CharField(max_length=255)
@@ -11,3 +14,7 @@ class Task(models.Model):
     deadline = models.DateTimeField(blank=True, null=True)
     completed = models.BooleanField(default=False)
     tags = models.ManyToManyField(to=Tag, related_name="tags")
+
+    class Meta:
+        ordering = ("completed", "-created")
+
