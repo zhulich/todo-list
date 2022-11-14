0d35e3379fa0a130aa984b16a03cc2427d0d47a7
Yevhenii Davydenko <akazhulich@gmail.com>
2022-11-14 14:44:15 +0200

    Add admin models for Task and Tag

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from todo.models import Task, Tag
+
+
 # Register your models here.
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    search_fields = ("content",)
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
